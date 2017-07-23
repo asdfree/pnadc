aa2c5cb1140d74c88944e9b58993c536f5a2766d
Anthony Damico <ajdamico@gmail.com>
2017-07-23 02:33:44 -0400

    rbind obviously

--- tests/setup.R
+++ tests/setup.R
@@ -10,6 +10,6 @@ pnadc_cat <-
 which_records <- sample( seq( nrow( pnadc_cat ) ) , round( nrow( pnadc_cat ) * 0.75 ) )
 
 # always sample year == 2015
-pnadc_cat <- unique( pnadc_cat[ which_records , ] , subset( pnadc_cat , year == 2015 ) )
+pnadc_cat <- unique( rbind( pnadc_cat[ which_records , ] , subset( pnadc_cat , year == 2015 ) ) )
 
 lodown( "pnadc" , pnadc_cat )
