bd712b512751ced4ffe151748a7cee2ff1a78cac
Anthony Damico <ajdamico@gmail.com>
2017-11-12 03:18:05 -0500

    fixes https://github.com/ajdamico/asdfree/issues/308

--- tests/setup.R
+++ tests/setup.R
@@ -1,15 +1,15 @@
 if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
 
+this_sample_break <- Sys.getenv( "this_sample_break" )
+
 library(lodown)
 
 pnadc_cat <-
 	get_catalog( "pnadc" ,
 		output_dir = file.path( getwd() ) )
 
-# sample 20% of the records
-which_records <- sample( seq( nrow( pnadc_cat ) ) , round( nrow( pnadc_cat ) * 0.20 ) )
+record_categories <- ceiling( seq( nrow( pnadc_cat ) ) / ceiling( nrow( pnadc_cat ) / 6 ) )
 
-# always sample year == 2015
-pnadc_cat <- unique( rbind( pnadc_cat[ which_records , ] , subset( pnadc_cat , year == 2015 ) ) )
+pnadc_cat <- unique( rbind( pnadc_cat[ record_categories == this_sample_break , ] , pnadc_cat[ pnadc_cat$year == 2015 , ] ) )
 
 lodown( "pnadc" , pnadc_cat )
