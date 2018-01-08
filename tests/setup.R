9f42847433965dd9df085b157e1c43f809a1e51c
Anthony Damico <ajdamico@gmail.com>
2018-01-08 07:51:20 -0500

    library(lodown)

--- tests/setup.R
+++ tests/setup.R
@@ -1,5 +1,6 @@
 if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
 
+library(lodown)
 this_sample_break <- Sys.getenv( "this_sample_break" )
 pnadc_cat <- get_catalog( "pnadc" , output_dir = file.path( getwd() ) )
 record_categories <- ceiling( seq( nrow( pnadc_cat ) ) / ceiling( nrow( pnadc_cat ) / 3 ) )
