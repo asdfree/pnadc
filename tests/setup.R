c261844e7e9047b7ce4c29f3bff3eeac07eff8fd
Anthony Damico <ajdamico@gmail.com>
2018-01-13 06:57:17 -0500

    savecache=FALSE and filesize_fun default

--- tests/setup.R
+++ tests/setup.R
@@ -1,5 +1,7 @@
 if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
 
+options("lodown.cachaca.savecache"=FALSE)
+
 library(lodown)
 this_sample_break <- Sys.getenv( "this_sample_break" )
 pnadc_cat <- get_catalog( "pnadc" , output_dir = file.path( getwd() ) )
